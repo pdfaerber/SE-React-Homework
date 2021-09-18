15aee79a8966b5da3308aa04c610b0a784cebb2c
pdfaerber <87340230+pdfaerber@users.noreply.github.com>
2021-09-18 17:06:38 -0400

    updating navbar components using react-router

--- myplants-app/src/components/pages/NavBar.js
+++ myplants-app/src/components/pages/NavBar.js
@@ -1,56 +1,83 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { MdFingerprint } from "react-icons/md";
-import { FaBars, FaTimes } from "react-icons/fa";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { MdFingerprint } from 'react-icons/md';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import {Button} from '../Button';
 
 function NavBar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    }else{
+      setButton(true);
+    }
+  } 
+  window.addEventListener('resize',showButton);
 
   return (
-    <div className="navbar">
-      <div className=".navbar-container container">
-        <MdFingerprint className="navbar-icon" />
-        <Link to="/" className="navbar-logo">
+    
+    <div className='navbar'>
+      <div className='.navbar-container container'>
+        <MdFingerprint className='navbar-icon' />
+        <Link to='/' className='navbar-logo'>
           Patrick's House (er... Apartment) of Plants
         </Link>
         <br />
-        <div className="menu-icon" onClick={handleClick}>
+        <div className='menu-icon' onClick={handleClick}>
           {click ? <FaTimes /> : <FaBars />}
         </div>
-        <ul className={click ? "nav-menu active" : "nav-menu"}>
-          <li className="nav-item">
-            <Link to="/" className="nav-links">
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links'>
               Home
             </Link>
           </li>
-          <li className="nav-item">
-            <Link to="/" className="nav-links">
+          <li className='nav-item'>
+            <Link to='/about' className='nav-links'>
               About
             </Link>
           </li>
-          <li className="nav-item">
-            <Link to="/" className="nav-links">
+          <li className='nav-item'>
+            <Link to='/meetmylants' className='nav-links'>
               Meet My Plants
             </Link>
           </li>
-          <li className="nav-item">
-            <Link to="/" className="nav-links">
+          <li className='nav-item'>
+            <Link to='/caretips' className='nav-links'>
               Care Tips/Advice
             </Link>
           </li>
-          <li className="nav-item">
-            <Link to="/" className="nav-links">
+          <li className='nav-item'>
+            <Link to='/donate' className='nav-links'>
               Donate
             </Link>
           </li>
-          <li className="nav-item">
-            <Link to="/" className="nav-links">
+          <li className='nav-item'>
+            <Link to='/contact' className='nav-links'>
               Contact
             </Link>
           </li>
-          
+          <li className='nav-btn'>
+            {button ? (
+            <Link to='/sign-up' className ='btn-link'>
+              <Button buttonStyle='btn--outline'
+                      buttonSize= 'btn-mobile'
+              >Sign Up</Button>
+            </Link>
+            ):(
+            <Link to='/sign-up' className ='btn-link'>
+              <Button buttonStyle='btn--outline'
+                      buttonSize= 'btn-mobile'
+              >Sign Up</Button>
+            </Link>
+            )}
+          </li>
         </ul>
       </div>
     </div>
